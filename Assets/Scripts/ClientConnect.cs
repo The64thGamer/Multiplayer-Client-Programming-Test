ee164ddcc7bad38b092815bc7678c70c85a0a28a
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2023-04-05 04:57:49 -0500

    Transfer client ID
    Client ID transferred through Unity prefab created when a client connects.

---
+++ Assets/Scripts/ClientConnect.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+
+public class ClientConnect : NetworkBehaviour
+{
+    /// <summary>
+    /// Transfers client info from Unity Netcode object spawned on Connect.
+    /// </summary>
+    public override void OnNetworkSpawn()
+    {
+        if (!IsHost) { Destroy(this.gameObject); }
+        GameObject.Find("Game Manager").GetComponent<NetcodeManager>().SpawnNewClient(OwnerClientId);
+    }
+}
